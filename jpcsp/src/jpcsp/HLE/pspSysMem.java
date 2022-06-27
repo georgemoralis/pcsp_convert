a9c6d17c8c738faa67853267adab68903340fc6e
georgemoralis <giorgosmrls@gmail.com>
2022-06-27 14:04:15 +0300

    JPCSP 16/9/2008 2a9c39b0

---
+++ jpcsp/src/jpcsp/HLE/pspSysMem.java
@@ -0,0 +1,252 @@
+/*
+Function:
+- http://psp.jim.sh/pspsdk-doc/group__SysMem.html
+
+Notes:
+Current allocation scheme doesn't handle partitions, freeing blocks or the
+space consumed by the program image.
+
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.HLE;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import jpcsp.Emulator;
+import jpcsp.GeneralJpcspException;
+import jpcsp.Memory;
+import jpcsp.MemoryMap;
+import static jpcsp.util.Utilities.*;
+/**
+ *
+ * @author shadow
+ */
+public class pspSysMem {
+    private static pspSysMem instance;
+
+    private static HashMap<Integer, SysMemInfo> blockList;
+    private int heapTop, heapBottom;
+
+    // PspSysMemBlockTypes
+    public static final int PSP_SMEM_Low = 0;
+    public static final int PSP_SMEM_High = 1;
+    public static final int PSP_SMEM_Addr = 2;
+    public static final int PSP_SMEM_LowAligned = 3;
+    public static final int PSP_SMEM_HighAligned = 4;
+    
+    private pspSysMem() { }
+
+    public static pspSysMem get_instance() {
+        if (instance == null) {
+            instance = new pspSysMem();
+        }
+        return instance;
+    }
+
+    public void Initialise(int programStartAddr, int programSize)
+    {
+        blockList = new HashMap<Integer, SysMemInfo>();
+
+        System.out.println("pspSysMem reserving " + programSize + " bytes at "
+                + String.format("%08x", programStartAddr) + " for app");
+
+        heapBottom = programStartAddr + programSize;
+        heapTop = MemoryMap.END_RAM;
+    }
+
+    // Allocates to 64-byte alignment
+    // TODO use the partitionid
+    public int malloc(int partitionid, int type, int size, int addr)
+    {
+        int allocatedAddress = 0;
+
+        // TODO check when we are running out of mem!
+        if (type == PSP_SMEM_Low)
+        {
+            allocatedAddress = heapBottom;
+            allocatedAddress = (allocatedAddress + 63) & ~63;
+            heapBottom = allocatedAddress + size;
+        }
+        else if (type == PSP_SMEM_Addr)
+        {
+            allocatedAddress = heapBottom;
+            if (allocatedAddress < addr)
+                allocatedAddress = addr;
+            allocatedAddress = (allocatedAddress + 63) & ~63;
+            heapBottom = allocatedAddress + size;
+        }
+        else if (type == PSP_SMEM_High)
+        {
+            allocatedAddress = (heapTop - (size + 63)) & ~63;
+            heapTop = allocatedAddress;
+        }
+        else if (type == PSP_SMEM_LowAligned)
+        {
+            allocatedAddress = heapBottom;
+            allocatedAddress = (allocatedAddress + addr - 1) & ~(addr - 1);
+            heapBottom = allocatedAddress + size;
+        }
+        else if (type == PSP_SMEM_HighAligned)
+        {
+            allocatedAddress = (heapTop - (size + addr - 1)) & ~(addr - 1);
+            heapTop = allocatedAddress;
+        }
+
+        return allocatedAddress;
+    }
+
+    // For internal use, example: ThreadMan allocating stack space
+    // Also removes the associated SysMemInfo (if found) from blockList
+    public void free(int addr)
+    {
+        boolean found = false;
+
+        // Reverse lookup on blockList, get SysMemInfo and call free
+        for (Iterator<SysMemInfo> it = blockList.values().iterator(); it.hasNext();)
+        {
+            SysMemInfo info = it.next();
+            if (info.addr == addr)
+            {
+                found = true;
+                free(info);
+                it.remove();
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            System.out.println("ERROR failed to map addr to SysMemInfo, possibly bad/missing cleanup or double free in HLE");
+        }
+    }
+
+    public void free(SysMemInfo info)
+    {
+        // TODO
+    }
+
+    public void sceKernelMaxFreeMemSize()
+    {
+        int maxFree = heapTop - heapBottom;
+        System.out.println("sceKernelMaxFreeMemSize " + maxFree
+                + " (hex=" + Integer.toHexString(maxFree) + ")");
+        Emulator.getProcessor().gpr[2] = maxFree;
+    }
+
+    public void sceKernelTotalFreeMemSize()
+    {
+        int totalFree = heapTop - heapBottom;
+        System.out.println("sceKernelTotalFreeMemSize " + totalFree
+                + " (hex=" + Integer.toHexString(totalFree) + ")");
+        Emulator.getProcessor().gpr[2] = totalFree;
+    }
+
+    /**
+     * @param partitionid TODO probably user, kernel etc
+     * 0 = ?
+     * 1 = kernel?
+     * 2 = user?
+     * @param type If type is PSP_SMEM_Addr, then addr specifies the lowest
+     * address allocate the block from.
+     */
+    public void sceKernelAllocPartitionMemory(int partitionid, int pname, int type, int size, int addr)
+    {
+        pname &= 0x3fffffff;
+        addr &= 0x3fffffff;
+        String name = readStringZ(Memory.get_instance().mainmemory, pname - MemoryMap.START_RAM);
+
+        // print debug info
+        String typeStr;
+        switch(type) {
+            case PSP_SMEM_Low: typeStr = "PSP_SMEM_Low"; break;
+            case PSP_SMEM_High: typeStr = "PSP_SMEM_High"; break;
+            case PSP_SMEM_Addr: typeStr = "PSP_SMEM_Addr"; break;
+            case PSP_SMEM_LowAligned: typeStr = "PSP_SMEM_LowAligned"; break;
+            case PSP_SMEM_HighAligned: typeStr = "PSP_SMEM_HighAligned"; break;
+            default: typeStr = "UNHANDLED " + type; break;
+        }
+        System.out.println("sceKernelAllocPartitionMemory(partitionid=" + partitionid
+                + ",name='" + name + "',type=" + typeStr + ",size=" + size
+                + ",addr=0x" + Integer.toHexString(addr) + ")");
+
+        addr = malloc(partitionid, type, size, addr);
+        if (addr != 0)
+        {
+            SysMemInfo info = new SysMemInfo(partitionid, name, type, size, addr);
+            Emulator.getProcessor().gpr[2] = info.uid;
+        }
+        else
+        {
+            Emulator.getProcessor().gpr[2] = -1;
+        }
+    }
+
+    public void sceKernelFreePartitionMemory(int uid) throws GeneralJpcspException
+    {
+        SceUIDMan.get_instance().checkUidPurpose(uid, "SysMem", true);
+        SysMemInfo info = blockList.remove(uid);
+        if (info == null) {
+            System.out.println("sceKernelFreePartitionMemory unknown SceUID=" + Integer.toHexString(uid));
+            Emulator.getProcessor().gpr[2] = -1;
+        } else {
+            free(info);
+            System.out.println("UNIMPLEMENT:sceKernelFreePartitionMemory SceUID=" + Integer.toHexString(info.uid) + " name:'" + info.name + "'");
+            Emulator.getProcessor().gpr[2] = 0;
+        }
+    }
+
+    public void sceKernelGetBlockHeadAddr(int uid) throws GeneralJpcspException
+    {
+        SceUIDMan.get_instance().checkUidPurpose(uid, "SysMem", true);
+        SysMemInfo info = blockList.get(uid);
+        if (info == null) {
+            System.out.println("sceKernelGetBlockHeadAddr unknown SceUID=" + Integer.toHexString(uid));
+            Emulator.getProcessor().gpr[2] = -1;
+        } else {
+            System.out.println("sceKernelGetBlockHeadAddr SceUID=" + Integer.toHexString(info.uid) + " name:'" + info.name + "' headAddr:" + Integer.toHexString(info.addr));
+            Emulator.getProcessor().gpr[2] = info.addr;
+        }
+    }
+
+    public void sceKernelDevkitVersion()
+    {
+        // Return 1.5 for now
+        System.out.println("sceKernelDevkitVersion 0x01050100");
+        Emulator.getProcessor().gpr[2] = 0x01050100;
+    }
+
+    class SysMemInfo {
+        public final int uid;
+        public final int partitionid;
+        public final String name;
+        public final int type;
+        public final int size;
+        public final int addr;
+
+        public SysMemInfo(int partitionid, String name, int type,
+                int size, int addr) {
+            this.partitionid = partitionid;
+            this.name = name;
+            this.type = type;
+            this.size = size;
+            this.addr = addr;
+
+            uid = SceUIDMan.get_instance().getNewUid("SysMem");
+            blockList.put(uid, this);
+        }
+
+    }
+}
---
+++ jpcsp/src/jpcsp/graphics/VertexInfo.java
@@ -0,0 +1,184 @@
+/*
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.graphics;
+
+import jpcsp.Emulator;
+import jpcsp.Memory;
+
+// Based on soywiz/pspemulator
+public class VertexInfo {
+    // vtype
+    public int transform2D;
+    public int skinningWeightCount;
+    public int morphingVertexCount;
+    public int texture;
+    public int color;
+    public int normal;
+    public int position;
+    public int weight;
+    public int index;
+
+    // vaddr, iaddr
+    public int ptr_vertex;
+    public int ptr_index;
+
+    // other data
+    public int vertexSize;
+
+    private static int[] size_mapping = new int[] { 0, 1, 2, 4 };
+
+    private static String[] texture_info = new String[] {
+        null, "GU_TEXTURE_8BIT", "GU_TEXTURE_16BIT", "GU_TEXTURE_32BITF"
+    };
+    private static String[] color_info = new String[] {
+        null, "GU_COLOR_UNK2", "GU_COLOR_UNK3", "GU_COLOR_UNK4",
+        "GU_COLOR_5650", "GU_COLOR_5551", "GU_COLOR_4444", "GU_COLOR_8888"
+    };
+    private static String[] normal_info = new String[] {
+        null, "GU_NORMAL_8BIT", "GU_NORMAL_16BIT", "GU_NORMAL_32BITF"
+    };
+    private static String[] vertex_info = new String[] {
+        null, "GU_VERTEX_8BIT", "GU_VERTEX_16BIT", "GU_VERTEX_32BITF"
+    };
+    private static String[] weight_info = new String[] {
+        null, "GU_WEIGHT_8BIT", "GU_WEIGHT_16BIT", "GU_WEIGHT_32BITF"
+    };
+    private static String[] index_info = new String[] {
+        null, "GU_INDEX_8BIT",  "GU_INDEX_16BIT", "GU_INDEX_UNK3"
+    };
+    private static String[] transform_info = new String[] {
+        "GU_TRANSFORM_3D", "GU_TRANSFORM_2D"
+    };
+
+    public void processType(int param) {
+        texture             = (param >>  0) & 0x3;
+        color               = (param >>  2) & 0x7;
+        normal              = (param >>  5) & 0x3;
+        position            = (param >>  7) & 0x3;
+        weight              = (param >>  9) & 0x3;
+        index               = (param >> 11) & 0x3;
+        skinningWeightCount = ((param >> 14) & 0x7) + 1;
+        morphingVertexCount = (param >> 18) & 0x3;
+        transform2D         = (param >> 23) & 0x1;
+
+        vertexSize = 0;
+        vertexSize += size_mapping[weight] * skinningWeightCount;
+        vertexSize += (color != 0) ? ((color == 7) ? 4 : 2) : 0;
+        vertexSize += size_mapping[texture] * 2;
+        vertexSize += size_mapping[position] * 3;
+        vertexSize += size_mapping[normal] * 3;
+    }
+
+    public int getAddress(int i) {
+        if (ptr_index != 0) {
+            int addr = ptr_index + i * index;
+            switch(index) {
+                case 1: i = Emulator.getMemory().read8(addr); break;
+                case 2: i = Emulator.getMemory().read16(addr); break;
+                case 4: i = Emulator.getMemory().read32(addr); break;
+            }
+        }
+
+        return ptr_vertex + i * vertexSize;
+    }
+
+    public VertexState readVertex(Memory mem, int addr) {
+        VertexState v = new VertexState();
+
+        switch(texture) {
+            case 1:
+                v.u = Emulator.getMemory().read8(addr); addr += 1;
+                v.v = Emulator.getMemory().read8(addr); addr += 1;
+                break;
+            case 2:
+                v.u = Emulator.getMemory().read16(addr); addr += 2;
+                v.v = Emulator.getMemory().read16(addr); addr += 2;
+                break;
+            case 3:
+                v.u = Float.intBitsToFloat(mem.read32(addr)); addr += 4;
+                v.v = Float.intBitsToFloat(mem.read32(addr)); addr += 4;
+                break;
+        }
+
+        switch(color) {
+            case 1: case 2: case 3: System.out.println("unimplemented color type"); addr += 1; break;
+            case 4: case 5: case 6: System.out.println("unimplemented color type"); addr += 2; break;
+            case 7: { // GU_COLOR_8888
+                int packed = mem.read32(addr); addr += 4;
+                v.r = (float)((packed      ) & 0xff) / 255;
+                v.g = (float)((packed >>  8) & 0xff) / 255;
+                v.b = (float)((packed >> 16) & 0xff) / 255;
+                v.a = (float)((packed >> 24) & 0xff) / 255;
+                break;
+            }
+        }
+
+        switch(normal) {
+            case 1:
+                v.nx = Emulator.getMemory().read8(addr); addr += 1;
+                v.ny = Emulator.getMemory().read8(addr); addr += 1;
+                v.nz = Emulator.getMemory().read8(addr); addr += 1;
+                break;
+            case 2:
+                v.nx = Emulator.getMemory().read16(addr); addr += 2;
+                v.ny = Emulator.getMemory().read16(addr); addr += 2;
+                v.nz = Emulator.getMemory().read16(addr); addr += 2;
+                break;
+            case 3:
+                v.nx = Float.intBitsToFloat(mem.read32(addr)); addr += 4;
+                v.ny = Float.intBitsToFloat(mem.read32(addr)); addr += 4;
+                v.nz = Float.intBitsToFloat(mem.read32(addr)); addr += 4;
+                break;
+        }
+
+        switch(position) {
+            case 1: System.out.println("unimplemented vertex type"); addr += 1; break;
+            case 2: System.out.println("unimplemented vertex type"); addr += 2; break;
+            case 3: { // GU_VERTEX_32BITF
+                v.px = Float.intBitsToFloat(mem.read32(addr)); addr += 4;
+                v.py = Float.intBitsToFloat(mem.read32(addr)); addr += 4;
+                v.pz = Float.intBitsToFloat(mem.read32(addr)); addr += 4;
+                break;
+            }
+        }
+
+        return v;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+
+        if (texture_info[texture] != null)
+            sb.append(texture_info[texture] + "|");
+        if (color_info[color] != null)
+            sb.append(color_info[color] + "|");
+        if (normal_info[normal] != null)
+            sb.append(normal_info[normal] + "|");
+        if (vertex_info[position] != null)
+            sb.append(vertex_info[position] + "|");
+        if (weight_info[weight] != null)
+            sb.append(weight_info[weight] + "|");
+        if (index_info[index] != null)
+            sb.append(index_info[index] + "|");
+        if (transform_info[transform2D] != null)
+            sb.append(transform_info[transform2D]);
+
+        sb.append(" size=" + vertexSize);
+        return sb.toString();
+    }
+}
